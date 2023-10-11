dd5e2f68a596f6f0c07ea8bbd9e97ca62f1c052d
s4llm4n <s4llm4n@gmail.com>
2023-10-11 21:23:00 +0700

    tampilan_home

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -3,6 +3,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:quranapps/app/contants/color.dart';
 import 'package:quranapps/app/data/models/surah.dart';
 import 'package:quranapps/app/routes/app_pages.dart';
 
@@ -15,37 +16,75 @@ class HomeView extends GetView<HomeController> {
       appBar: AppBar(
         title: Text('My Quran'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {}, 
+            icon: Icon(Icons.search),
+          ),
+        ],
       ),
-      body: FutureBuilder<List<Surah>>(
-          future: controller.getAllSurah(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            if (!snapshot.hasData) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            return ListView.builder(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
-                  Surah surah = snapshot.data![index];
-                  return ListTile(
-                    onTap: () {
-                      Get.toNamed(Routes.DETAIL_SURAH, arguments: surah);
-                    },
-                    leading: CircleAvatar(
-                      child: Text("${surah.number}"),
-                    ),
-                    title: Text("${surah.name?.transliteration?.id}"),
-                    subtitle: Text("${surah.numberOfVerses} Ayat | ${surah.revelation?.id}"),
-                    trailing: Text("${surah.name?.short}"),
-                  );
-                });
-          }),
+
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Assalamualaikum',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20,),
+            Container(
+              height: 150,
+              width: Get.width,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                  colors: [
+                    appPurpleLight2,
+                    appPurpleDark,
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      // body: FutureBuilder<List<Surah>>(
+      //     future: controller.getAllSurah(),
+      //     builder: (context, snapshot) {
+      //       if (snapshot.connectionState == ConnectionState.waiting) {
+      //         return Center(
+      //           child: CircularProgressIndicator(),
+      //         );
+      //       }
+      //       if (!snapshot.hasData) {
+      //         return Center(
+      //           child: CircularProgressIndicator(),
+      //         );
+      //       }
+      //       return ListView.builder(
+      //           itemCount: snapshot.data!.length,
+      //           itemBuilder: (context, index) {
+      //             Surah surah = snapshot.data![index];
+      //             return ListTile(
+      //               onTap: () {
+      //                 Get.toNamed(Routes.DETAIL_SURAH, arguments: surah);
+      //               },
+      //               leading: CircleAvatar(
+      //                 child: Text("${surah.number}"),
+      //               ),
+      //               title: Text("${surah.name?.transliteration?.id}"),
+      //               subtitle: Text("${surah.numberOfVerses} Ayat | ${surah.revelation?.id}"),
+      //               trailing: Text("${surah.name?.short}"),
+      //             );
+      //           });
+      //     },
+      //   ),
     );
   }
 }
