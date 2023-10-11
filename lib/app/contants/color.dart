00d1139b916742781a719291d6f1c0469de951b5
s4llm4n <s4llm4n@gmail.com>
2023-10-11 09:27:21 +0700

    ubahwarna

--- lib/app/contants/color.dart
+++ lib/app/contants/color.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
 
 const appPurple = Color(0xFF431AA1);
+const appPurpleDark = Color(0xFF1E0771);
 const appPurpleLight1 = Color(0xFF9345F2);
 const appPurpleLight2 = Color(0xFFB9A2DB);
 const appWhite = Color(0xFFFAF8FC);
 const appOrange = Color(0xFFE6704A);
+
+ThemeData applight = ThemeData(
+  primaryColor: appPurple,
+  scaffoldBackgroundColor: appPurple,
+  appBarTheme: AppBarTheme(
+    backgroundColor: appPurple,
+  ),
+);
+ThemeData appDark = ThemeData(
+  primaryColor: appPurple,
+  scaffoldBackgroundColor: appPurpleDark,
+  appBarTheme: AppBarTheme(
+    backgroundColor: appPurpleDark,
+  ),
+);
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:quranapps/app/contants/color.dart';
 
 import 'app/routes/app_pages.dart';
 
 void main() {
   runApp(
     GetMaterialApp(
+      theme: appDark,
       title: "Application",
       initialRoute: Routes.INTRODUCTION,
       getPages: AppPages.routes,
